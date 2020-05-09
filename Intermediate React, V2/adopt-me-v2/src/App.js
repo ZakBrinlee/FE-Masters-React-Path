693e910d7e2ff73084853a76eeb8a2622666a378
ZakBrinlee <zbrinlee@hotmail.com>
2020-05-09 16:49:32 -0700

    CSS in JS with Emotion

--- Intermediate React, V2/adopt-me-v2/src/App.js
+++ Intermediate React, V2/adopt-me-v2/src/App.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, use } from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
   const themeHook = useState("");
   return (
     <ThemeContext.Provider value={themeHook}>
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
+        <NavBar />
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
---
+++ Intermediate React, V2/adopt-me-v2/src/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: "#bf3334",
+  secondary: "#d9c148",
+  dark: "#333",
+  light: "#81a69b",
+  darkgrey: "#878787",
+};
